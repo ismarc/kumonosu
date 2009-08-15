ae22396eef7e664975791ffdcc70d296a53651ea
Matt Brace <ismarc31@gmail.com>
2009-08-14 23:04:07 -0500

    Updated documentation for new classes and regenerated doxygen documentation

--- src/clientlib/Client.h
+++ src/clientlib/Client.h
@@ -25,22 +25,123 @@ using namespace apache::thrift::protocol;
 using namespace apache::thrift::transport;
 
 namespace kumonosu {
+    //! Provides an interface for a service to communicate.
+    /*!
+     *  \class Client
+     *
+     *  Client provides an interface for a service to communicate via
+     *  a server with services running locally or remotely,
+     *  transparent to the Client.  A service can use Client directly,
+     *  or can use a convenience class for handling message processing
+     *  (see MessageProcessor).
+     *
+     *  \author Matt Brace
+     */
     class Client {
     public:
+        //! Initializes the client
+        /*!
+         *  Initializes a client to process messages destined for the
+         *  supplied service id.  Uses localhost:9191 for the server.
+         *  \param serviceId the unique identifier for the service.
+         */
         Client(int32_t serviceId);
+        //! Initializes the client
+        /*!
+         *  Initializes a client to process messages destined for the
+         *  supplied service id.  Uses a hostname of localhost.
+         *  \param serviceId the unique identifier for the service.
+         *  \param serverPort the port on localhost to connect to.
+         */
         Client(int32_t serviceId, int32_t serverPort);
+        //! Initializes the client
+        /*!
+         *  Initializes a client to process messages destined for the
+         *  supplied service id.
+         *  \param serviceId the unique identifier for the service.
+         *  \param serverAddress the IP or hostname of the server to
+         *  connect to.
+         *  \param serverPort the port on the server to connect to.
+         */
         Client(int32_t serviceId, std::string serverAddress, int32_t serverPort);
         ~Client();
-        
+
+        //! Opens a connection to the server
+        /*!
+         *  Client is intended to hold a connection open for extended
+         *  periods of time (not on-demand for requests).  connect()
+         *  establishes the connection.
+         */
         void connect();
+        //! Closes a connection to the server
+        /*!
+         *  Closes the connection to the server, used if the service
+         *  needs to change details about the connection or close
+         *  outgoing sockets.
+         */
         void disconnect();
+        //! Assigns the server IP or hostname to connect to.
+        /*!
+         *  Closes the connection if open and assigns the specified
+         *  server address.  The connection is not reopened
+         *  afterwards.
+         *  \param serverAddress the IP or hostname to connect to.
+         */
         void setServerAddress(std::string serverAddress);
+        //! Assigns the server port to connect to.
+        /*!
+         *  Closes the connection if open and assigns the specified
+         *  port to connect to.  The connection is not reopened
+         *  afterwards.
+         *  \param serverPort the port on the server to connect to.
+         */
         void setServerPort(int32_t serverPort);
-        
+
+        //! Sends a queue item request to the server.
+        /*!
+         *  Sends a queue item request to the assigned server for
+         *  dispatching to another service (as defined by the local
+         *  handler interface).
+         *  \param serviceId the unique identifier of the service to
+         *  send the item to.
+         *  \param item the queue item to the service.
+         *  \return true if successful, false otherwise.
+         */
         bool sendRequest(int32_t serviceId, queueItem item);
+        //! Sends a queue item request destined for the local manager.
+        /*!
+         *  Sends a request destined for the local manager to
+         *  process.  Note that this is not used for local services,
+         *  this is intended for communication with the manager
+         *  itself.
+         *  \param item the queue item to the local server.
+         *  \return true if successful, false otherwise.
+         */
         bool sendLocalRequest(queueItem item);
+        //! Retrieves the next item from the queue.
+        /*!
+         *  Retrieves the next item in the queue for the service id
+         *  associated with this client.
+         *  \return the queueItem for the next item or NULL if no
+         *  items are in the queue.
+         */
         queueItem getNextPendingItem();
+        //! Retrieves all outstanding items from the queue.
+        /*!
+         *  Retrieves all items pending in the queue for the service
+         *  id associated with this client.
+         *  \return the queueItemList of pending items or an empty
+         *  queueItemList.
+         */
         queueItemList getPendingItems();
+        //! Retrieves a maximum number of outstanding items from the
+        //! queue.
+        /*!
+         *  Retrieves a maximum of count items from the queue for the
+         *  service id associated with this client.
+         *  \return the queueItemList of pending items or an empty
+         *  queueItemList
+         */
         queueItemList getPendingItems(int32_t count);
     private:
         std::string _serverAddress;
--- src/clientlib/MessageProcessor.h
+++ src/clientlib/MessageProcessor.h
@@ -16,20 +16,81 @@
 #include "Callback.h"
 
 namespace kumonosu {
+    //! Provides a convenience interface for managing pending items.
+    /*!
+     *  \class MessageProcessor
+     *
+     *  The message processor provides a convenience class for
+     *  handling incoming queue items.  A class (or classes) can be
+     *  created to handle the different methods which are then
+     *  provided to the message processor as callbacks.  The service
+     *  can then call the MessageProcessor to execute any outstanding
+     *  items.
+     *
+     *  \author Matt Brace
+     */
     class MessageProcessor {
     public:
+        //! Initializes the MessageProcessor
+        /*!
+         *  The constructor takes an object pointer and a function
+         *  pointer for the callback that is called whenever a method
+         *  callback has not been set for the received method id.
+         *  \param methodNotImplementedObject the object to supply in
+         *  the callback when a request for an undefined method is
+         *  received.
+         *  \param methodNotImplemented function pointer to the method
+         *  to call when an undefined method is received.
+         */
         // Should really typedef this to look better
         MessageProcessor(void* methodNotImplementedObject,
                          void (*methodNotImplemented)(void* object,
                                                       arguments argList));
         ~MessageProcessor() {};
 
+        //! Assigns the supplied client
+        /*!
+         *  To retreive requests, a client is necessary.  It is the
+         *  owning objects responsibility to initialize and supply the
+         *  client.  Once received, the MessageProcessor will have the
+         *  client connect if necessary for further calls, but will
+         *  never explicitly disconnect the client.
+         *  \param client the client object to use for message retrieval.
+         */
         void setClient(Client* client);
+        //! Stores a callback function associated with a method id.
+        /*!
+         *  Creates a Callback object and associates it internally
+         *  with the supplied method id.  The callback will be called
+         *  with there is an incoming request with the specified
+         *  method id.
+         *  \param methodId the method id to associate the callback
+         *  with.
+         *  \param object the object to call with the function pointer
+         *  \param FunctionPtr the function pointer for the callback.
+         */
         void setMethodCallback(int32_t methodId, void* object,
                                void(*FunctionPtr)
                                (void* object,
                                 arguments argList));
+        //! Removes a Callback object from the method associations.
+        /*!
+         *  Removes the Callback object that is associated with the
+         *  method id.  If further requests are processed with that
+         *  method id, the methodNotImplemented callback will be used
+         *  until a new callback has been set.
+         *  \param methodId the method id of the callback to remove.
+         */
         void removeMethodCallback(int32_t methodId);
+        //! Processes outstanding items.
+        /*!
+         *  Retrieves up to max_count items and calls the associated
+         *  callback method with the received method id for each
+         *  item.  The callbacks are called serially, in the order
+         *  they are received based on the queue.
+         *  \param max_count the maximum number of items to process,
+         *  use -1 to indicate all items.
+         */
         void processPendingItems(int32_t max_count);
     private:
         void executeMethod(int32_t methodId, arguments argList);
